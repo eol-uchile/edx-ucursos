332e7036792820d0a606466cf943909309914e74
fsologureng <fsologur@uchile.cl>
2024-07-18 18:47:59 -0400

    ci: centralize external workflow

--- .github/test.sh
+++ .github/test.sh
@@ -1,20 +1,22 @@
 #!/bin/dash
-pip install -e git+https://github.com/eol-uchile/uchileedxlogin@d6bc551f2516aafd723b742dd9cd3dee103a17b5#egg=uchileedxlogin
-pip install -e /openedx/requirements/edx-ucursos
 
-cd /openedx/requirements/edx-ucursos
+set -e
+
+pip install -e git+https://github.com/eol-uchile/uchileedxlogin@master#egg=uchileedxlogin
+pip install -e /openedx/requirements/app
+pip install pytest-cov genbadge[coverage]
+
+cd /openedx/requirements/app
 cp /openedx/edx-platform/setup.cfg .
+sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=edxucursos/ --cov-report term-missing --cov-report xml:reports/coverage/coverage.xml --cov-fail-under 70' setup.cfg
+
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/edx-ucursos
-
-pip install pytest-cov genbadge[coverage]
-sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=edxucursos/ --cov-report term-missing --cov-report xml:reports/coverage/coverage.xml --cov-fail-under 70' setup.cfg
+cd /openedx/requirements/app
 
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest edxucursos/tests.py
 
-rm -rf test_root
-
+echo -e "[run]\nomit = edxucursos/migrations/*" > .coveragerc
 genbadge coverage
