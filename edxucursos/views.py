2b3160b5fcc6cbea4b3178c884a9364b52c4af0c
jjorgeb <jorge.barraza@uchile.cl>
2025-09-03 15:40:37 -0400

    refactor!: updates compatibility with new version of uchileedxlogin, using its service layer
    - adds course enrollemnt and validation functions that used to be imported from uchileedxlogin
    - improves functions descriptions, comments and variable names.
    - adds exception handling for edxloginuser_factory and removes doc_id validation from validate_data.

--- edxucursos/views.py
+++ edxucursos/views.py
@@ -14,79 +14,95 @@ import uuid
 from django.conf import settings
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
-from django.db import transaction
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.urls import reverse
 from django.views.generic.base import View
 from rest_framework_jwt.settings import api_settings
 from rest_framework_jwt.utils import jwt_get_secret_key
-from uchileedxlogin.models import EdxLoginUser
-from uchileedxlogin.views import EdxLoginStaff
+from uchileedxlogin.services.interface import edxloginuser_factory, EmailException, get_user_by_doc_id, PhApiException
 import jwt
 import requests
 import six
+
+# Edx dependencies
+from common.djangoapps.student.models import CourseEnrollment
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.keys import CourseKey
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 
 # Internal project dependencies
 from .models import EdxUCursosMapping
 
 logger = logging.getLogger(__name__)
-msg_error = "contáctese al correo eol-ayuda@uchile.cl adjuntando el número del error"
+MSG_ERROR = "contáctese al correo eol-ayuda@uchile.cl adjuntando el número del error"
 
 class EdxUCursosLoginRedirect(View):
     """
-        Return a url with user token to log in
+    Return a url with user token to log in.
     """
     def get(self, request):
+        error_id = str(uuid.uuid4())
         ticket = request.GET.get('ticket', "")
         logger.info('ticket: ' + ticket)
         user_data = self.get_data_ticket(ticket)
         if user_data['result'] == 'error':
-            id_error = str(uuid.uuid4())
-            logger.error(id_error + '- Data error')
+            logger.error(error_id + '- Data error')
             logger.info(user_data)
             return HttpResponseNotFound(
-                '(Error '+ id_error +') Error con la api de ucursos (ticket), por favor '+ msg_error)
+                '(Error '+ error_id +') Error con la api de ucursos (ticket), por favor '+ MSG_ERROR)
 
         if self.verify_caducity(user_data):
-            id_error = str(uuid.uuid4())
-            logger.error(id_error + ' - Ticket caducado: ' + ticket)
+            logger.error(error_id + ' - Ticket caducado: ' + ticket)
             return HttpResponseNotFound(
-                '(Error '+ id_error +') Ticket caducado, reintente nuevamente o '+ msg_error)
+                '(Error '+ error_id +') Ticket caducado, reintente nuevamente o '+ MSG_ERROR)
         if 'id_externo' in user_data:
-            rut = user_data['id_externo']
+            doc_id = user_data['id_externo']
+        # If the user doesn't have an id_externo it is assumed that its pers_id is a rut without the verification digit.
         else:
-            rut = str(user_data['pers_id'])
-            rut_dv = self.digito_verificador(rut)
-            while len(rut) < 9:
-                rut = "0" + rut
-            rut = rut + rut_dv
-
+            # Rut formatting.
+            doc_id = str(user_data['pers_id'])
+            ver_digit = self.verification_digit(doc_id)
+            while len(doc_id) < 9:
+                doc_id = "0" + doc_id
+            doc_id = doc_id + ver_digit
         u_course = self.get_edxucursos_mapping(user_data['grupo'])
-        mapp_course = self.validate_data(rut, u_course)
+        mapp_course = self.validate_data(u_course)
         if not mapp_course:
-            id_error = str(uuid.uuid4())
-            logger.error(id_error + '- Error con los parametros: rut de usuario o id del curso')
+            logger.error(error_id + '- Error con el parametro: id del curso')
             return HttpResponseNotFound(
-                '(Error '+ id_error +') Error con los parametros: rut de usuario o id del curso, por favor '+ msg_error)
-
+                '(Error '+ error_id +') Error con el parametro: id del curso, por favor '+ MSG_ERROR)
         mode = self.get_mode(user_data["permisos"])
-        edxlogin_user = self.enroll_or_create_user(rut, mapp_course, mode)
-
-        if edxlogin_user:
-            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-            payload = self.get_payload(edxlogin_user.user, u_course)
-            token = jwt_encode_handler(payload)
-            return HttpResponse(
-                EdxUCursosLoginRedirect.get_callback_url(request, token))
-        else:
-            id_error = str(uuid.uuid4())
-            logger.error(id_error + ' - Error creating user')
-            return HttpResponseNotFound(
-                '(Error '+ id_error +') Error con los datos del usuario, por favor '+ msg_error)
+        edxlogin_user = get_user_by_doc_id(doc_id)
+        if not edxlogin_user:
+            try:
+               edxlogin_user = edxloginuser_factory(doc_id, "doc_id")
+            except ValueError:
+                logger.error(f'{error_id} - Error when trying to create edxloginuser with doc_id: {doc_id}')
+                return HttpResponseNotFound(
+                    f'(Error {error_id}) Error con la validacion del doc_id del usuario, por favor {MSG_ERROR}')
+            except PhApiException:
+                logger.error(f'{error_id} - Error when trying to create edxloginuser with doc_id: {doc_id}')
+                return HttpResponseNotFound(
+                    f'(Error {error_id}) Error con la obtencion de datos desde ph para el usuario, por favor {MSG_ERROR}')
+            except EmailException:
+                logger.error(f'{error_id} - Error when trying to create edxloginuser with doc_id: {doc_id}')
+                return HttpResponseNotFound(
+                    f'(Error {error_id}) Error con los correos del usuario, por favor {MSG_ERROR}')
+            except Exception:
+                logger.error(f'{error_id} - Error when trying to create edxloginuser with doc_id: {doc_id}')
+                return HttpResponseNotFound(
+                    f'(Error {error_id}) Error con los datos del usuario, por favor {MSG_ERROR}')
+        # Enroll the user.
+        CourseEnrollment.enroll(edxlogin_user.user, CourseKey.from_string(str(mapp_course.edx_course)), mode=mode)
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = self.get_payload(edxlogin_user.user, u_course)
+        token = jwt_encode_handler(payload)
+        return HttpResponse(
+            EdxUCursosLoginRedirect.get_callback_url(request, token))
 
     def get_data_ticket(self, ticket):
         """
-            Get user data through the ticket
+        Get user data through the ticket.
         """
         parameters = {
             'ticket': ticket
@@ -109,16 +125,16 @@ class EdxUCursosLoginRedirect(View):
 
     def verify_caducity(self, data):
         """
-            Check if ticket is expired
+        Check if the ticket is expired.
         """
         aux_time = time.time()
         if (aux_time - data["time"]) > 60:
             return True
         return False
 
-    def digito_verificador(self, rut):
+    def verification_digit(self, rut):
         """
-            Return rut check digit
+        Calculate the verification digit of a rut.
         """
         revertido = list(map(int, reversed(str(rut))))
         factors = cycle(list(range(2, 8)))
@@ -129,10 +145,20 @@ class EdxUCursosLoginRedirect(View):
             return 'K'
         
         return str(res)
+    
+    def validate_course(self, course_id):
+        """
+        Verify if a course associated with course_id exists.
+        """
+        try:
+            course_key = CourseKey.from_string(course_id)
+            return CourseOverview.objects.filter(id=course_key).exists()
+        except InvalidKeyError:
+            return False
 
     def get_edxucursos_mapping(self, data):
         """
-            Return id_ucursos 
+        Return id_ucursos.
         """
         return '{}/{}/{}/{}/{}'.format(data['base'],
                                        data['anno'],
@@ -143,7 +169,7 @@ class EdxUCursosLoginRedirect(View):
 
     def get_payload(self, user, course):
         """
-            Create payload with user data to create auth token
+        Create payload with user data to create auth token.
         """
         payload = {'username': user.username, 'user_id': user.id, 'exp': dt.utcnow(
         ) + datetime.timedelta(seconds=settings.EDXUCURSOS_EXP_TIME), 'course': course}
@@ -153,27 +179,11 @@ class EdxUCursosLoginRedirect(View):
 
         return payload
 
-    def validate_data(self, rut, course):
+    def validate_data(self, course):
         """
-            Verify if rut and course are correct
+        Verify if the course exists and if its associated to a Ucursos course.
         """
-        try:
-            if rut[0] == 'P':
-                if 5 > len(rut[1:]) or len(rut[1:]) > 20:
-                    logger.error("Rango de rut pasaporte debe ser mayor a 5 y menor a 20, {}".format(rut))
-                    return False
-            elif rut[0:2] == 'CG':
-                if len(rut) != 10:
-                    logger.error("Rango de rut CG debe ser 10, {}".format(rut))
-                    return False
-            else:
-                if not EdxLoginStaff().validarRut(rut):
-                    logger.error("Rut invalido en EdxLoginStaff().validarRut(rut): {}".format(rut))
-                    return False
-        except Exception:
-            logger.error("Rut invalido: {}".format(rut))
-            return False
-
+        # Checks if there is a mapping between course and a ucursos course.
         try:
             course = EdxUCursosMapping.objects.get(
                 ucurso_course=course)
@@ -181,43 +191,17 @@ class EdxUCursosLoginRedirect(View):
         except EdxUCursosMapping.DoesNotExist:
             logger.error("No Existe EdxUCursosMapping, id: {}".format(course))
             return False
-
-        # verify if exists course
-        if not EdxLoginStaff().validate_course(course_id):
+        # Checks if the course exists.
+        if not self.validate_course(course_id):
             logger.error("Curso no existe, id: {}".format(course_id))
             return False
-
         return course
-
-    def enroll_or_create_user(self, run, course, mode):
-        """
-            Get o create edxlogin_user and enroll to course
-        """
-        with transaction.atomic():
-            try:
-                edxlogin_user = EdxLoginUser.objects.get(run=run)
-                EdxLoginStaff().enroll_course(
-                    edxlogin_user, six.text_type(
-                        course.edx_course), True, mode)
-                logger.info(
-                    'Exists EdxLogin_User: ' +
-                    edxlogin_user.user.username)
-                return edxlogin_user
-            except EdxLoginUser.DoesNotExist:
-                logger.info('Force Create User')
-                edxlogin_user = EdxLoginStaff().force_create_user(run)
-                if edxlogin_user:
-                    EdxLoginStaff().enroll_course(
-                        edxlogin_user, six.text_type(
-                            course.edx_course), True, mode)
-                    return edxlogin_user
-        return None
 
     def get_mode(self, data):
         """
-            Return mode data.
-            audit == Profesor
-            honor == Estudiante
+        Return mode data.
+        audit == Profesor
+        honor == Estudiante
         """
         if "PROFESOR" in data and data["PROFESOR"] == 1:
             return "audit"
@@ -228,7 +212,7 @@ class EdxUCursosLoginRedirect(View):
     @staticmethod
     def get_callback_url(request, token):
         """
-        Get the callback url
+        Get the callback url.
         """
         if settings.EDXUCURSOS_DOMAIN != "":
             url = '{}{}'.format(settings.EDXUCURSOS_DOMAIN, reverse('edxucursos-login:callback'))
@@ -239,32 +223,32 @@ class EdxUCursosLoginRedirect(View):
 
 class EdxUCursosCallback(View):
     """
-        Login user if token is valid
+    Login user if token is valid.
     """
     def get(self, request):
         token = request.GET.get('token', "")
         logger.info('token: ' + token)
 
-        #decode token
+        # Decode token.
         try:
             payload = self.decode_token(token)
         except jwt.ExpiredSignatureError:
             id_error = str(uuid.uuid4())
             logger.error(id_error +' - Caducity Ticket')
             return HttpResponseNotFound(
-                '(Error '+ id_error +') Ticket caducado, reintente nuevamente o '+ msg_error)
+                '(Error '+ id_error +') Ticket caducado, reintente nuevamente o '+ MSG_ERROR)
         except Exception:
             id_error = str(uuid.uuid4())
             logger.error(id_error + ' - Decoding failure')
             return HttpResponseNotFound(
-                '(Error '+ id_error +') Error en la decoficación, reintente nuevamente o '+ msg_error)
-        #verify if course parameter exists
+                '(Error '+ id_error +') Error en la decoficación, reintente nuevamente o '+ MSG_ERROR)
+        # Verify if course parameter exists.
         if 'course' not in payload:
             id_error = str(uuid.uuid4())
             logger.error(id_error + ' - Decoding failure: No Course')
             return HttpResponseNotFound(
-                '(Error '+ id_error +') Error en la decoficación (parametro: curso), reintente nuevamente o '+ msg_error)
-        #verify course_id exists
+                '(Error '+ id_error +') Error en la decoficación (parametro: curso), reintente nuevamente o '+ MSG_ERROR)
+        # Verify course_id exists.
         try:
             course = EdxUCursosMapping.objects.get(
                 ucurso_course=payload['course'])
@@ -273,7 +257,7 @@ class EdxUCursosCallback(View):
             id_error = str(uuid.uuid4())
             logger.error(id_error + ' - El curso no se ha vinculado con un curso de eol')
             return HttpResponseNotFound(
-                '(Error '+ id_error +') El curso no se ha vinculado con un curso de eol, por favor '+ msg_error)
+                '(Error '+ id_error +') El curso no se ha vinculado con un curso de eol, por favor '+ MSG_ERROR)
 
         try:
             login_user = User.objects.get(id=payload['user_id'])
@@ -291,11 +275,11 @@ class EdxUCursosCallback(View):
             id_error = str(uuid.uuid4())
             logger.error(id_error + ' - Logging Error')
             return HttpResponseNotFound(
-                '(Error '+ id_error +') Logging Error, reintente nuevamente o '+ msg_error)
+                '(Error '+ id_error +') Logging Error, reintente nuevamente o '+ MSG_ERROR)
 
     def decode_token(self, token):
         """
-            Decode token
+        Decode token.
         """
         options = {
             'verify_exp': True,
