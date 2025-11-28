f312dd625bb4ba9a0b928e0e85051a74c3d3d596
jjorgeb <jorge.barraza@uchile.cl>
2025-11-28 15:40:20 -0300

    chore: update version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="edxucursos",
-    version="1.0.0",
+    version="2.0.0",
     author="EOL UChile",
     author_email="eol-ing@uchile.cl",
     description="Authentication backend for EOl from Ucursos",
